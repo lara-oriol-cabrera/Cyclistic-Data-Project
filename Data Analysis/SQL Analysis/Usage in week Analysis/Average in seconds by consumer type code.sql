db31b1c5bd7833fa2c7284b5367e65c88ea65b29
Lara Oriol Cabrera <laraoricab@gmail.com>
2024-09-25 15:33:38 +0200

    Delete Data Analysis/SQL Analysis/Usage in week Analysis/Average in seconds by consumer type code.sql

--- Data Analysis/SQL Analysis/Usage in week Analysis/Average in seconds by consumer type code.sql
+++
@@ -1,4 +0,0 @@
-SELECT Member_casual,
-AVG(EXTRACT(HOUR FROM Ride_Length)*3600 + EXTRACT(MINUTE FROM Ride_Length)* 60 + EXTRACT(SECOND FROM Ride_Length)) AS Average_Ride_Length_inSeconds
-FROM `subtle-backup-427612-a4.projectcyclistic.cyclistictable` 
-GROUP BY Member_casual
