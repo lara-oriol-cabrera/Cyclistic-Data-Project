fe5d7481e233478adf406c9045c3a0a73af51f53
Lara Oriol Cabrera <laraoricab@gmail.com>
2024-09-25 15:25:43 +0200

    Update and rename Data Analysis/SQL Analysis/Usage in the week code.sql to Data Analysis/SQL Analysis/Usage in week Analysis/Usage in the week code.sql

--- Data Analysis/SQL Analysis/Usage in the week code.sql
+++
@@ -1,6 +0,0 @@
-SELECT Day_of_the_week,
-Member_casual,
-COUNT(*) AS Total_IDs
- FROM `subtle-backup-427612-a4.projectcyclistic.cyclistictable` 
- GROUP BY Day_of_the_week,
- Member_casual
---
+++ Data Analysis/SQL Analysis/Usage in week Analysis/Usage in the week code.sql
@@ -0,0 +1,6 @@
+SELECT Day_of_the_week,
+Member_casual,
+COUNT(*) AS Total_IDs
+FROM `subtle-backup-427612-a4.projectcyclistic.cyclistictable` 
+GROUP BY Day_of_the_week,
+ Member_casual
