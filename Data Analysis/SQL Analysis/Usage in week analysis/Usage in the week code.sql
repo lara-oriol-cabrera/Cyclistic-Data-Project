124ca291ef91e97a6c9ccd86ecc79a0fb8a31d2f
Lara Oriol Cabrera <laraoricab@gmail.com>
2024-09-28 18:43:26 +0200

    Usage in the week code.sql

--- Data Analysis/SQL Analysis/Usage in week analysis/Usage in the week code.sql
+++ Data Analysis/SQL Analysis/Usage in week analysis/Usage in the week code.sql
@@ -1,6 +1,6 @@
 SELECT Day_of_the_week,
 Member_casual,
 COUNT(*) AS Total_IDs
- FROM `subtle-backup-427612-a4.projectcyclistic.cyclistictable` 
- GROUP BY Day_of_the_week,
- Member_casual
+FROM `subtle-backup-427612-a4.projectcyclistic.cyclistictable` 
+GROUP BY Day_of_the_week,
+Member_casual
